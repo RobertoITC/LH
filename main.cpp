86c6c3ae15d6fa2e757c0bd010a7b0f7918847b5
RobertoITC <111032606+RobertoITC@users.noreply.github.com>
2024-04-24 20:13:21 -0600

    final main.cpp

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main() {
     ofstream outFile("/Users/robertomoralesnieto/CLionProjects/LHmaybe/output.html");
 
     if (!inFile.is_open()) {
-        cerr << "Failed to open input.txt" << endl;
+        cerr << "No se pudo abrir input.txt" << endl;
         return 1;
     }
 
@@ -29,7 +29,7 @@ int main() {
                    ".function { color: yellow; }\n"
                    "</style>\n</head>\n<body>\n<pre>\n";
 
-    std::regex reOperators(R"([\+\-\*]|\<\<|\>\>)");
+    std::regex reOperators(R"([\+\-\*]|\<\<|\>\>|[^<]\/[^s])");
     std::regex reConditionals(R"(\b(if|else)\b)");
     std::regex reLoops(R"(\b(while|for)\b)");
     std::regex reComments(R"(\/\/.*[^\n])");
@@ -38,32 +38,32 @@ int main() {
     std::regex reDatatypes(R"(\b(int|float|bool)\b)");
     std::regex reAccessKeywords(R"(\b(public|private|protected)\b)");
     std::regex reFunctions(R"(\b(\w+)\s*\()");
-    std::regex reSlash(R"([^<]\/[^s])");
+    //std::regex reSlash(R"([^<]\/[^s])");
 
     while (getline(inFile, line)) {
         size_t commentPos = line.find("//");
-        string codeBeforeComment;
+        string lineaTemporal;
 
         if (commentPos != string::npos) {
-            codeBeforeComment = line.substr(0, commentPos);
-            line = codeBeforeComment + "<span class='comment'>" + line.substr(commentPos) + "</span>";
+            lineaTemporal = line.substr(0, commentPos);
+            line = lineaTemporal + "<span class='comment'>" + line.substr(commentPos) + "</span>";
         } else {
-            codeBeforeComment = line;
+            lineaTemporal = line;
         }
-        codeBeforeComment = std::regex_replace(codeBeforeComment, reStrings, "<span class='string'>$&</span>");
-        codeBeforeComment = std::regex_replace(codeBeforeComment, reSlash,"<span class='operator'>$&</span>");
-        codeBeforeComment = std::regex_replace(codeBeforeComment, reOperators, "<span class='operator'>$&</span>");
-        codeBeforeComment = std::regex_replace(codeBeforeComment, reConditionals, "<span class='conditional'>$&</span>");
-        codeBeforeComment = std::regex_replace(codeBeforeComment, reLoops, "<span class='loop'>$&</span>");
-        codeBeforeComment = std::regex_replace(codeBeforeComment, reBooleans, "<span class='boolean'>$&</span>");
-        codeBeforeComment = std::regex_replace(codeBeforeComment, reDatatypes, "<span class='datatype'>$&</span>");
-        codeBeforeComment = std::regex_replace(codeBeforeComment, reAccessKeywords, "<span class='access'>$&</span>");
-        codeBeforeComment = std::regex_replace(codeBeforeComment, reFunctions, "<span class='function'>$1</span>(");
+        lineaTemporal = std::regex_replace(lineaTemporal, reStrings, "<span class='string'>$&</span>");
+        //lineaTemporal = std::regex_replace(lineaTemporal, reSlash,"<span class='operator'>$&</span>");
+        lineaTemporal = std::regex_replace(lineaTemporal, reOperators, "<span class='operator'>$&</span>");
+        lineaTemporal = std::regex_replace(lineaTemporal, reConditionals, "<span class='conditional'>$&</span>");
+        lineaTemporal = std::regex_replace(lineaTemporal, reLoops, "<span class='loop'>$&</span>");
+        lineaTemporal = std::regex_replace(lineaTemporal, reBooleans, "<span class='boolean'>$&</span>");
+        lineaTemporal = std::regex_replace(lineaTemporal, reDatatypes, "<span class='datatype'>$&</span>");
+        lineaTemporal = std::regex_replace(lineaTemporal, reAccessKeywords, "<span class='access'>$&</span>");
+        lineaTemporal = std::regex_replace(lineaTemporal, reFunctions, "<span class='function'>$1</span>(");
 
         if (commentPos != string::npos) {
-            line = codeBeforeComment + line.substr(codeBeforeComment.size());
+            line = lineaTemporal + line.substr(lineaTemporal.size());
         } else {
-            line = codeBeforeComment;
+            line = lineaTemporal;
         }
         htmlContent << line << '\n';
     }
